ec2feaca7efd6adfc76a627ecf74e1b883590b08
Ajiharan <baskaranajiharan1243@gmail.com>
2021-08-25 13:09:59 +0530

    login part added

--- app/src/main/java/com/example/funlap/MainActivity.java
+++ app/src/main/java/com/example/funlap/MainActivity.java
@@ -6,32 +6,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_home,btn_view;
+    private Button btn_login;
+    private EditText email_address,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_home=findViewById(R.id.btn_homeScreen);
-        btn_view=findViewById(R.id.btn_view);
-        btn_view.setOnClickListener(new View.OnClickListener() {
+        btn_login=findViewById(R.id.btn_login);
+        email_address=findViewById(R.id.email_address);
+        password=findViewById(R.id.password);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+    }
+    private void loginUser(){
+        if(!email_address.getText().toString().isEmpty() && !password.getText().toString().isEmpty()){
+            if(email_address.getText().toString().equals("admin@gmail.com") && password.getText().toString().equals("111111")){
+                Intent intent=new Intent(MainActivity.this,AdminHomeScreen.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }else{
                 Intent intent=new Intent(MainActivity.this,UserHomeScreen.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(intent);
             }
-        });
-        btn_home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,AdminHomeScreen.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                startActivity(intent);
+        }else{
+            Toast.makeText(this,"Some fields are missing",Toast.LENGTH_SHORT).show();
+        }
 
-            }
-        });
     }
 }
